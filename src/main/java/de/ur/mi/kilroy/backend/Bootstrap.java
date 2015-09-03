81dc854f10bb84ed4867b8b8812c03ca096bb0de
simon <noreply@simongeyer.com>
2015-09-03 10:25:49 +0200

    fixed env variables

--- src/main/java/de/ur/mi/kilroy/backend/Bootstrap.java
+++ src/main/java/de/ur/mi/kilroy/backend/Bootstrap.java
@@ -14,23 +14,23 @@ public class Bootstrap {
     private static final Logger logger = LoggerFactory.getLogger(Bootstrap.class.getCanonicalName());
 
     private static final String IP_ADDRESS = System.getenv("OPENSHIFT_DIY_IP") != null ? System.getenv("OPENSHIFT_DIY_IP") : "localhost";
-    private static final int PORT = System.getenv("OPENSHIFT_DIY_IP") != null ? Integer.parseInt(System.getenv("OPENSHIFT_DIY_IP")) : 8080;
+    private static final int PORT = System.getenv("OPENSHIFT_DIY_PORT") != null ? Integer.parseInt(System.getenv("OPENSHIFT_DIY_PORT")) : 8080;
 
     public static void main(String[] args) throws Exception {
         ipAddress(IP_ADDRESS);
         port(PORT);
         staticFileLocation("/public");
 
-        String host = System.getenv("OPENSHIFT_MONGODB_DB_HOST");
+        String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
         int port = 3306;
         String dbname = "kilroy_db";
         String username = "kilroy";
         String password = "kilroywashere";
         if (host != null) {
-            port = Integer.parseInt(System.getenv("OPENSHIFT_MONGODB_DB_PORT"));
+            port = Integer.parseInt(System.getenv("OPENSHIFT_MYSQL_DB_PORT"));
             dbname = System.getenv("OPENSHIFT_APP_NAME");
-            username = System.getenv("OPENSHIFT_MONGODB_DB_USERNAME");
-            password = System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD");
+            username = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+            password = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
         } else {
             host = "localhost";
         }
